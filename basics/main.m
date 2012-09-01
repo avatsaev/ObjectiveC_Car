3b55688e6611afd0d7851f2111b160f0e9c92cc5
avatsaev <azero_79@hotmail.com>
2012-09-01 21:45:14 +0200

    Initial Commit

---
+++ basics/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  basics
+//
+//  Created by A. Vatsaev  on 01/09/2012.
+//  Copyright (c) 2012 A. Vatsaev . All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
